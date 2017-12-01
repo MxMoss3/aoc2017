9c9d1bf3fbc1804c37d8c872a9c8019f9f64d48e
MxMoss3 <mossberg.max@gmail.com>
2017-12-01 00:59:52 -0500

    day 1 complete

--- 1_day.py
+++ 1_day.py
@@ -1,14 +1,11 @@
 with open("1_day.txt") as f:
-    text = f.read()
-
-    text = list(text) 
+    text = list(f.read())
     text.pop()
+
     text.append(text[0])
     tot = 0
-    good = []
     for i in range(len(text) - 1):
         if text[i] == text[i+1]:
-            good.append(text[i])
             tot += int(text[i])
         else:
             None
