f69a8ba17f4b7e8d8ee306820077139aa070af5e
MxMoss3 <mossberg.max@gmail.com>
2017-12-06 01:06:55 -0500

    day 6 complete

---
+++ 6_day_pt2.py
@@ -0,0 +1,25 @@
+raw = "4    10  4   1   8   4   9   14  5   1   14  15  0   15  3   5"
+
+text = [int(x) for x in raw.split(" ") if x != '']
+
+loop = True
+combos = []
+
+while loop:
+
+    high = max(text)
+    ind = (text.index(high) + 1) % len(text)
+    text[text.index(high)] = 0
+    while high > 0:
+        text[ind] += 1
+        ind = (ind + 1) % len(text)
+        high -= 1
+    
+    if text in combos:
+        a = combos.index(text)
+        inf = len(combos)-a
+        loop = False
+
+    combos.append([x for x in text])
+        
+print(inf)
